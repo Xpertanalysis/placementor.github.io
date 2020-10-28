59163170cfe98b1422bea12942417dcaaeb51154
placementor <64643756+placementor@users.noreply.github.com>
2020-10-28 22:37:29 +0530

    PlaceMentor Landing Page

---
+++ js/home_name.js
@@ -0,0 +1,37 @@
+
+// // Get the video
+// var video = document.getElementById("myVideo");
+
+
+
+
+// var vid = document.getElementById("myVideo");
+// vid.oncanplaythrough = function() {
+//     var e = document.getElementById("video_cover");
+//        e.style.display = 'none'
+// };
+
+
+var line = document.querySelector(".line");
+
+setTimeout(() => {
+    line.innerHTML = " Unlock your doors towards your dream Placement."
+}, 3000);
+
+
+
+// var placementor = document.querySelector(".placementor");
+
+// var w = ["P","L","A","C","E","M","E","N","T","O","R",0];
+
+
+// setInterval(() =>{
+//     for (let i = 0; i< w.length; i++){
+//       if (typeof w[i]== "string") {
+//         placementor.innerHTML = w[i]
+   
+//       } 
+      
+//     }
+// },1000)
+
